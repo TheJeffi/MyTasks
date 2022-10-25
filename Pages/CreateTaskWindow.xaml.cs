7c32f3b79d55d2e3fe302c83368d7f204d7b8349
Jagermeister <TheJeffi@yandex.ru>
2022-10-25 10:39:28 +0300

    Реализовано окно создания задачи

---
+++ Pages/CreateTaskWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyTasks.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для CreateTaskWindow.xaml
+    /// </summary>
+    public partial class CreateTaskWindow : Window
+    {
+        public CreateTaskWindow()
+        {
+            InitializeComponent();
+
+            CreateTaskButton.Click += Create_Task;
+
+            GetTaskData();
+        }
+
+        private void Create_Task(object sender, RoutedEventArgs e)
+        {
+            GetTaskData();
+        }
+
+        private void GetTaskData()
+        {
+            var taskName = TaskName.Text.Trim();
+            var taskStartDate = TaskCreateDate.Text.Trim();
+            var taskEndDate = TaskEndDate.Text.Trim();
+            var taskPuprose = TaskPurpose.Text.Trim();
+
+            MessageBox.Show($"{taskName}, {taskStartDate}, {taskEndDate}, {taskPuprose}");
+        }
+    }
+}
